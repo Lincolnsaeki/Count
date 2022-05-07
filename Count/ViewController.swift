09240c3569c514a3707081df9d85bd37e9371cfe
Rinnosuke Saeki <macrin1559@gmail.com>
2022-05-07 18:55:38 +0900

    Initial Commit

---
+++ Count/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Count
+//
+//  Created by 佐伯凜乃介 on 2022/04/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var countLabel: UILabel!
+    @IBOutlet var tapButton: UIButton!
+    
+    var tapCount = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+tapButton.layer.cornerRadius = 125
+        
+    }
+    
+    @IBAction func tapTapButton(){
+        
+        tapCount = tapCount + 1
+        
+        countLabel.text = String(tapCount)
+        
+    }
+
+}
+
